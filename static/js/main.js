ceede7c0df716685ff325e439ef791f93500596a
eric323c <71191021+eric323c@users.noreply.github.com>
2024-11-02 07:26:22 -0400

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,8 @@
+document.getElementById('create-post').addEventListener('click', function () {
+    document.querySelector('.modal').style.display = 'flex';
+});
+
+document.querySelector('.close-modal').addEventListener('click', function () {
+    document.querySelector('.modal').style.display = 'none';
+});
 
