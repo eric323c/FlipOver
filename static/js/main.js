0ff08662ffe066a8d76c9e332d4eafa0c671c1f3
eric323c <71191021+eric323c@users.noreply.github.com>
2024-11-16 05:45:51 -0500

    Delete static directory

--- static/js/main.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('create-post').addEventListener('click', function () {
-    document.querySelector('.modal').style.display = 'flex';
-});
-
-document.querySelector('.close-modal').addEventListener('click', function () {
-    document.querySelector('.modal').style.display = 'none';
-});
-
